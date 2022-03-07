4cd197bab7d2a5d84f472cd460d20491f63effe5
Pervez Shoaib <50441829+PervezSH@users.noreply.github.com>
2022-03-07 20:21:53 +0530

    create first contract 🥳

---
+++ 01-first-smart-contract/contracts/HelloWorld.sol
@@ -0,0 +1,8 @@
+// tell Solidity, what compiler you expect
+pragma solidity ^0.8.0;
+
+contract HelloWorld {
+    function hello() public pure returns (string memory) {
+        return "Hello, World";
+    }
+}
