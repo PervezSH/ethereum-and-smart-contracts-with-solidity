02f749101220b5fb64078559bb9f6e389f455baa
Pervez Shoaib <50441829+PervezSH@users.noreply.github.com>
2022-03-13 20:50:22 +0530

    feat(game): create hero contract ✨

---
+++ 03-game-contract/contracts/Here.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.8.0;
+
+contract Hero {
+    enum Class {
+        Mage,
+        Healer,
+        Barbarian
+    }
+
+    function createHero(Class class) public payable {
+        require(msg.value >= 0.05 ether, "Please send more money!!!");
+    }
+}
