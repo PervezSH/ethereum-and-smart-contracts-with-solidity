f198e6af4a3e3104ceaf6c49f7c4e8926bd0598a
Pervez Shoaib <50441829+PervezSH@users.noreply.github.com>
2022-03-16 20:26:13 +0530

    test(game): write a test 🚨
    - for createHero func

---
+++ 03-game-contract/test/Hero.ts
@@ -0,0 +1,31 @@
+import "@nomiclabs/hardhat-ethers";
+import { ethers } from "hardhat";
+import { expect } from "chai";
+import { Contract } from "ethers";
+
+describe("Hero", function() {
+    async function deployHero() {
+        const Hero = await ethers.getContractFactory("Hero");
+        const hero = await Hero.deploy();
+        await hero.deployed();
+
+        return hero;
+    }
+    let hero : Contract;
+    // runs once before the first test in this block
+    before(async function () {
+        hero = await deployHero();
+    });
+    it("should fail at creating hero cause of payment", async function () {
+        let e : any;
+        try {
+            await hero.createHero(0, {
+                value: ethers.utils.parseEther("0.0499999999")
+            });
+        } catch (err) {
+            e = err;
+        }
+
+        expect(e.message.includes("Please send more money!!!")).to.equal(true);
+    })
+});
